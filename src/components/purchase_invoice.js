01c7920628585d850f47f4d25fae2edea9eb53ad
PrathamDeshmukh <prathamdeshmukh86@gmail.com>
2024-04-29 13:26:20 +0530

    some changes

--- src/components/purchase_invoice.js
+++ src/components/purchase_invoice.js
@@ -34,24 +34,141 @@ export default class Purchase_invoice extends Component {
   </div>
   <div className="row">
     <div className="col-md-12 grid-margin transparent">
-      <div className="card ">
+      <div className="card">
         <div className="card-body">
           <div className="row">
-            <div className="col-md-6 mb-4 stretch-card transparent">
-              <input type="text" className="form-control p-2" placeholder="Search invoice by Number, Distributor and more" defaultValue />
+            <div className="col-md-8 mb-4 stretch-card transparent">
+              <input type="text" className="form-control p-2" name="tags" id="tags" placeholder="Filter Here" defaultValue />
             </div>
-            <div className="col-md-3 mb-4 stretch-card transparent">
-              <button type="button" className="btn btn-link btn-fw">Pending Payment Invoices</button>
+            <div className="col-md-2 mb-4 stretch-card transparent">
+              <button type="button" className="btn btn-primary float-end">Pending purchase </button>
             </div>
-            <div className="col-md-3 mb-4 stretch-card transparent">
-              {/* <button type="button" class="btn btn-link btn-fw">+ New Purchase Invoices</button> */}
-              <a className="btn btn-link btn-fw" href="<?php echo base_url();?>invoice_setting">+  New Purchase Invoices</a>
+            <div className="col-md-2 mb-4 transparent">
+          
+              <button type="button" className="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+               Add Purchase Invoice
+        </button>
+         {/* Modal */}
+     <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog  ">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h3 className="card-description"> Add New Stock</h3>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+      </div>
+      <div className="modal-body">
+
+
+
+
+
+
+
+
+     <div className="row">
+  <div className="col-md-12 grid-margin">
+    {/* Product table will be loaded here */}
+    <div className="card">
+      <div className="card-body">
+        <form className="form-sample" >
+         
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Employee Full Name:</label>
+                <input type="text" name="e_name" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Contact Number:</label>
+                <input type="text" name="e_phone" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
             </div>
           </div>
-        </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Create Password:</label>
+                <input type="password" name="password" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Confirm Password:</label>
+                <input type="password" name="c_pass" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Age:</label>
+                <input type="text" name="e_age" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Aadhar Number:</label>
+                <input type="text" name="adhar_no" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label htmlFor="exampleInputUsername1">Address:</label>
+                <input type="text" name="e_address" className="form-control form-control-sm" id="exampleInputUsername1" />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label htmlFor="exampleFormControlSelect1">Allow sales Permissions:</label>
+                <select name="sale_permission" className="form-control form-control-sm" id="exampleFormControlSelect1">
+                  <option>Select</option>
+                  <option>Yes</option>
+                  <option>No</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlSelect2">Allow Purchase Permissions:</label>
+                <select name="purchase_permission" className="form-control form-control-sm" id="exampleFormControlSelect2">
+                  <option>Select</option>
+                  <option>Yes</option>
+                  <option>No</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <button type="submit" id="product-btn" className="submit-button btn btn-primary ">Add </button>
+        </form>
       </div>
     </div>
   </div>
+</div>
+
+
+
+
+        
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+            </div>
+          </div>
+          
   <div className="row">
     <div className="col-md-12 grid-margin stretch-card">
       <div className="card">
@@ -107,7 +224,10 @@ export default class Purchase_invoice extends Component {
     </div>
   </div>
 </div>
-
+</div>
+</div>
+</div>
+</div>
 
     );
 }
